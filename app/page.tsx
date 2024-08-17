990c600fa46c5dd79fdc152242c7cd86bed223a1
Harshit Gupta <hersheyg0105@gmail.com>
2024-08-17 19:51:58 -0400

    updated build issues

--- app/page.tsx
+++ app/page.tsx
@@ -99,7 +99,7 @@ export default function Home() {
           <div className="mx-auto mt-16 max-w-7xl px-6 sm:mt-20 md:mt-24 lg:px-8">
             <dl className="mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-10 text-base leading-7 text-gray-600 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16">
               {features.map((feature) => (
-                <div className="relative pl-9">
+                <div key={feature.name} className="relative pl-9">
                   <dt className="inline font-semibold text-gray-900">
                     <feature.icon
                       aria-hidden="true"
--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -36,7 +36,7 @@ function FileUploader() {
           ) : (
             <>
               <CircleArrowDown className="h-20 w-20 animate-bounce"></CircleArrowDown>
-              <p>Drag 'n' drop some files here, or click to select files</p>
+              <p>Drag n drop some files here, or click to select files</p>
             </>
           )}
         </div>
