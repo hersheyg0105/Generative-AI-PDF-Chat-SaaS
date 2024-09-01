f535798d55504b5298ac28bf5330e638dad60742
Harshit Gupta <hersheyg0105@gmail.com>
2024-08-31 21:16:44 -0400

    initial pdf showing on screen

--- app/dashboard/files/[id]/page.tsx
+++ app/dashboard/files/[id]/page.tsx
@@ -1,13 +1,41 @@
 import React from "react";
+import { auth } from "@clerk/nextjs/server";
+import { adminDB } from "@/firebaseAdmin";
+import PdfView from "@/components/PdfView";
 
-function ChatToFilePage({
+async function ChatToFilePage({
   params: { id },
 }: {
   params: {
     id: string;
   };
 }) {
-  return <div>ChatToFilePage: {id}</div>;
+  auth().protect();
+  const { userId } = await auth();
+
+  const ref = await adminDB
+    .collection("users")
+    .doc(userId!)
+    .collection("files")
+    .doc(id)
+    .get();
+
+  const url = ref.data()?.downloadUrl;
+
+  return (
+    <div className="grid lg:grid-cols-5 h-full overflow-hidden">
+      {/* Right */}
+      <div className="col-span-5 lg:col-span-2 overflow-y-auto">
+        {/* Chat */}
+      </div>
+
+      {/* Left Section */}
+      <div className="col-span-5 lg:col-span-3 bg-gray-100 border-r-2 lg:border-indigo-600 lg:-order-1 overflow-auto">
+        {/* PDF View */}
+        <PdfView url={url} />
+      </div>
+    </div>
+  );
 }
 
 export default ChatToFilePage;
---
+++ components/PdfView.tsx
@@ -0,0 +1,56 @@
+"use client";
+import React from "react";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
+
+import { Document, Page, pdfjs } from "react-pdf";
+import { useEffect, useState } from "react";
+import { Button } from "./ui/button";
+import { Loader2Icon, RotateCw, ZoomInIcon, ZoomOutIcon } from "lucide-react";
+
+// Had to run this command below in google cloud terminal
+// gsutil cors set cors.json gs://chat-with-pdf-7156d.appspot.com
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+
+function PdfView({ url }: { url: string }) {
+  const [numPages, setNumPages] = useState<number>(0);
+  const [pageNumber, setPageNumber] = useState<number>(1);
+  const [file, setFile] = useState<Blob | null>(null);
+  const [rotation, setRotation] = useState<number>(0);
+  const [scale, setScale] = useState<number>(1);
+
+  useEffect(() => {
+    const fetchFile = async () => {
+      const response = await fetch(url);
+      const file = await response.blob();
+      setFile(file);
+    };
+    console.log("I am in PdfView use effect");
+    fetchFile();
+  }, [url]);
+
+  const onDocumentLoadSuccess = ({ numPages }: { numPages: number }): void => {
+    setNumPages(numPages);
+  };
+
+  return (
+    <div>
+      {!file ? (
+        <Loader2Icon className="animate-spin h-20 w-20 text-indigo-600 mt-20" />
+      ) : (
+        <Document
+          loading={null}
+          file={file}
+          rotate={rotation}
+          onLoadSuccess={onDocumentLoadSuccess}
+          className="m-4 overflow-scroll"
+        >
+          <Page className="shadow-lg" scale={scale} pageNumber={pageNumber} />
+        </Document>
+      )}
+    </div>
+  );
+}
+
+export default PdfView;
